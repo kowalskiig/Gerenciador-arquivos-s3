39c735cd28469e1268af766abc25919a4c2f90d5
Gustavo Eiji Kowalski Hatada <kkowalskigustavo@gmail.com>
2025-10-08 16:05:08 -0300

    feat: uploading image to S3 commandlinerunner

--- src/main/java/com/projeto/s3/S3Application.java
+++ src/main/java/com/projeto/s3/S3Application.java
@@ -1,12 +1,24 @@
 package com.projeto.s3;
 
+import com.projeto.s3.service.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class S3Application {
+public class S3Application implements CommandLineRunner {
+
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(S3Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        s3Service.uploadFile("c:\\Users\\nawsz\\Downloads\\images.jpg");
     }
 }
